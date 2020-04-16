15615a77c17f46d11f648320076f49c58ec438fb
Khaled FERJANI <kferjani@linagora.com>
2020-04-16 17:38:12 +0200

    linagora/lgs/smartsla/smartsla-frontend#536 fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 git config --global user.name "CI Bot"
 git config --global user.email "bot@linagora.com"
